50801b5215c8d53decb0f43f2e9f2f0d9cb771c7
Mikhail Ovcharenko <90459699+Tormgor@users.noreply.github.com>
2021-11-11 19:39:02 +0300

    Add files via upload

---
+++ newsapp/templatetags/custom_filters.py
@@ -0,0 +1,12 @@
+
+from django import template
+
+register = template.Library()  # регистрация фильтра
+
+censor_list = ['слово1', 'слово2', 'слово3', 'слово4'] # словарь нецензурных слов
+
+@register.filter(name='censor')
+def censor(value):
+    for word in censor_list:
+        value = value.replace(word, '***') # замены слова на ***
+    return value
